4747e14a5758a41bc9721dd6df52d46022ddee28
r_tonouchi <r_tonouchi@mstool.net>
2024-12-20 16:39:08 +0900

    success url

--- src/app/lib/stripe/checkout.ts
+++ src/app/lib/stripe/checkout.ts
@@ -6,15 +6,16 @@ import getStripeInstance from './client';
 export async function createCheckoutSession(stripePriceId: string): Promise<Stripe.Checkout.Session> {
   const stripe = getStripeInstance();
   const APP_BASE_URL = process.env.APP_BASE_URL;
-  if (!APP_BASE_URL) {
-    throw new Error('APP_BASE_URL environment variable is not set.');
+  const SUCCESS_URL = process.env.SUCCESS_URL;
+  if (!APP_BASE_URL || !SUCCESS_URL) {
+    throw new Error('APP_BASE_URL or SUCCESS_URL environment variable is not set.');
   }
 
   const sessionParams: Stripe.Checkout.SessionCreateParams = {
     payment_method_types: ['card'],
     line_items: [{ price: stripePriceId, quantity: 1 }],
     mode: 'subscription',
-    success_url: `${APP_BASE_URL}/dashboard`,
+    success_url: `${SUCCESS_URL}`,
     cancel_url: `${APP_BASE_URL}/dashboard/pricing`,
   };
 
